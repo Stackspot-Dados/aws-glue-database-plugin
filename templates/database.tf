97dc7498e5465b86766b81b163c587f4a1d70aa8
Danielle Martins <danielle.silva@zup.con.br>
2022-08-15 19:48:03 -0300

    adiciona aspas

--- templates/database.tf
+++ templates/database.tf
@@ -1,5 +1,5 @@
 resource "aws_glue_catalog_database" "{{inputs.database_name}}" {
   name        = "{{inputs.database_name}}"
   catalog_id  = "${data.aws_caller_identity.current.account_id}"
-  description = {{inputs.description}}"
+  description = "{{inputs.description}}"
 }
